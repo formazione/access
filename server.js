6500f7b5c2eb1e5a9baab27b6dbb5dcf5538dc09
Glitch (accesspage) <none>
2019-11-10 07:46:09 +0000

    🚴😇 Checkpoint ./views/john.html:1262331/32 ./server.js:1262331/461 ./views/sara.html:1262331/1823

--- server.js
+++ server.js
@@ -8,17 +8,29 @@ var app = express();
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-var userName = process.env.USERNAME;
-var userPass = process.env.PASSWORD;
+var userName1 = process.env.USERNAME1;
+var userPass1 = process.env.PASSWORD1;
+
+var userName2 = process.env.USERNAME2;
+var userPass2 = process.env.PASSWORD2;
 
 var basicAuth = require("basic-auth");
+
 app.use(function(request, response, next) {
   var user = basicAuth(request);
   console.log(user);
-  if (!user || user.name !== userName || user.pass !== userPass) {
+  
+  
+  if (!user || user.name !== userName1 || user.pass !== userPass1) {
     response.set("WWW-Authenticate", 'Basic realm="example"');
     return response.status(401).send();
   }
+  
+  else if (!user || user.name !== userName2 || user.pass !== userPass2) {
+    response.set("WWW-Authenticate", 'Basic realm="example"');
+    return response.status(401).send();
+  }
+  
   return next();
 });
 
@@ -27,11 +39,18 @@ app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 // DIFFERENT PAGES FOR EACH NAME
-if (userName == "john"){
+if (userName1 == "john"){
   app.get("/", function(request, response) {
     response.sendFile(__dirname + "/views/john.html");
   });
 }
+
+else if (userName2 == "sara"){
+  app.get("/", function(request, response) {
+    response.sendFile(__dirname + "/views/sara.html");
+  });
+}
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
