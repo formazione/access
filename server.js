e2ca32b9c583d1dec9fa38783474a70b91bf37e3
Glitch (accesspage) <none>
2019-11-10 07:34:12 +0000

    🍨🗻 Checkpoint ./package.json:1262331/3937 ./server.js:1262331/2232 ./views/john.html:1262331/1819

--- server.js
+++ server.js
@@ -2,35 +2,38 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
-// we've started you off with Express, 
+// we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-var userName = "duder";
+var userName = process.env.USERNAME;
 var userPass = process.env.PASSWORD;
 
-var basicAuth = require('basic-auth');
-app.use(function (request, response, next) {
+var basicAuth = require("basic-auth");
+app.use(function(request, response, next) {
   var user = basicAuth(request);
   console.log(user);
   if (!user || user.name !== userName || user.pass !== userPass) {
-    response.set('WWW-Authenticate', 'Basic realm="example"');
+    response.set("WWW-Authenticate", 'Basic realm="example"');
     return response.status(401).send();
   }
   return next();
 });
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+// DIFFERENT PAGES FOR EACH NAME
+if (userName == "john"){
+  app.get("/", function(request, response) {
+    response.sendFile(__dirname + "/views/john.html");
+  });
+}
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
